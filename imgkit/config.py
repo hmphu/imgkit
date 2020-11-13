f53377f1940180e1ae4bfa807afed738bb2349ea
hmphu <hmphu.it@gmail.com@@users.noreply.github.com>
2020-11-14 00:55:47 +0700

    Enabled passing path to xvfb-run through config

--- imgkit/config.py
+++ imgkit/config.py
@@ -4,12 +4,10 @@ import sys
 
 
 class Config(object):
-    def __init__(self, wkhtmltoimage='', meta_tag_prefix='imgkit-'):
+    def __init__(self, wkhtmltoimage='', xvfb=None, meta_tag_prefix='imgkit-'):
         self.meta_tag_prefix = meta_tag_prefix
 
         self.wkhtmltoimage = wkhtmltoimage
-
-        self.xvfb = ''
 
         if not self.wkhtmltoimage:
             if sys.platform == 'win32':
@@ -18,13 +16,6 @@ class Config(object):
             else:
                 self.wkhtmltoimage = subprocess.Popen(['which', 'wkhtmltoimage'],
                                                       stdout=subprocess.PIPE).communicate()[0].strip()
-        if not self.xvfb:
-            if sys.platform == 'win32':
-                self.xvfb = subprocess.Popen(['where', 'xvfb-run'],
-                                             stdout=subprocess.PIPE).communicate()[0].strip()
-            else:
-                self.xvfb = subprocess.Popen(['which', 'xvfb-run'],
-                                             stdout=subprocess.PIPE).communicate()[0].strip()
 
         try:
             with open(self.wkhtmltoimage):
@@ -34,3 +25,19 @@ class Config(object):
                           'If this file exists please check that this process can '
                           'read it. Otherwise please install wkhtmltopdf - '
                           'http://wkhtmltopdf.org\n'.format(self.wkhtmltoimage))
+
+        self.xvfb = xvfb
+        if self.xvfb is not None and not self.xvfb:
+            if sys.platform == 'win32':
+                self.xvfb = subprocess.Popen(['where', 'xvfb-run'],
+                                            stdout=subprocess.PIPE).communicate()[0].strip()
+            else:
+                self.xvfb = subprocess.Popen(['which', 'xvfb-run'],
+                                            stdout=subprocess.PIPE).communicate()[0].strip()
+            try:
+                with open(self.xvfb):
+                    pass
+            except IOError:
+                raise IOError('No xvfb executable found: "{0}"\n'
+                                'If this file exists please check that this process can '
+                                'read it. Otherwise please install xvfb -'.format(self.xvfb))
